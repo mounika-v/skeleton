fc2378cb07f433a31e666e95c70bd21b2a65562a
mounika-v <mounika721@gmail.com>
2018-07-05 12:35:56 +0530

    With build files

---
+++ project/build/CMakeFiles/example_bin.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mounika/CPP/sem/sem/main.cpp" "/home/mounika/CPP/sem/sem/build/CMakeFiles/example_bin.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/mounika/CPP/sem/libigl/shared/cmake/../../include"
+  "/home/mounika/CPP/sem/libigl/shared/cmake/../../external/eigen"
+  "/home/mounika/CPP/sem/libigl/external/glad/include"
+  "/home/mounika/CPP/sem/libigl/external/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/mounika/CPP/sem/sem/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/home/mounika/CPP/sem/sem/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
